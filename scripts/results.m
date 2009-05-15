a7a0ab0049f24ea1d9866a9a77bc581c74aa8e57
Caio Tiago Oliveira <asrail@gmail.com>
2009-05-14 22:53:41 -0300

    Mudando o nome do arquivo onde salvar as caracteristicas

--- scripts/results.m
+++ scripts/results.m
@@ -5,5 +5,5 @@ files = dir('*p*');
 for i = 1:length(files)
   tmp = struct2cell(carac(files(i).name));
   vetor = [tmp{:}];
-  save('test', 'vetor', '-ASCII', '-append');
+  save('results', 'vetor', '-ASCII', '-append');
 end
